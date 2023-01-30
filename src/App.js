a1b09e3bc075fd2ed3e5eb0c32d85db37bc68a6b
Alejae1998 <alejagr1998@gmail.com>
2023-01-30 14:12:07 -0800

    app redirecting properly

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import backgroundImage from './shopping-cart.avif';
 import Auth from './components/Auth/Auth.js';
+import { useUser } from './context/userContext.js';
 
 function App() {
+  const { user } = useUser();
   return (
     <div className="App" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <Switch>
         <Route path="/auth/:type" component={Auth} />
+        <>{!user && <Redirect to="/auth/sign-in" />}</>
       </Switch>
     </div>
   );
